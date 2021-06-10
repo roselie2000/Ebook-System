b57ccbd549228a0c1c047a5278ee4b2dd6d7e4ca
roselie2000 <84838548+roselie2000@users.noreply.github.com>
2021-06-10 09:09:55 -0700

    Add files via upload

---
+++ Administrator.py
@@ -0,0 +1,163 @@
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+import mysql.connector
+
+class Administrator:
+    def __init__(self):
+        self.__Username = "101RGG"
+        self.__Password = "Rose2000"
+        self.con = mysql.connector.connect(host="localhost", port=3306, user="root", password="Roselie@2000", database="Ebooks")
+        self.cur = self.con.cursor()
+
+    def tkin1(self):
+        self.root = tk.Tk()
+        self.user = tk.StringVar()
+        self.pwd = tk.StringVar()
+        self.root.title("Administrator")
+        self.root.geometry('500x300')
+        self.root.iconbitmap('C:/image/book_icon.ico')
+        bg = tk.PhotoImage(file="C:/image/background1.png")
+        self.label1 = tk.Label(self.root, image=bg)
+        self.label1.place(x=0, y=0)
+
+        self.lab1 = ttk.Label(self.root, text="Username : ", background="#61626B", font=("Times",14,"bold italic"), foreground="white").grid(row = 0,column = 0, pady=5, padx=5)
+        self.entry1 = ttk.Entry(self.root, textvariable=self.user).grid(row = 0, column = 1)
+        self.lab2 = ttk.Label(self.root, text="Password : ",background="#61626B", font=("Times",14,"bold italic"), foreground="white").grid(row=2, column=0, pady=5, padx=5)
+        self.entry2 = ttk.Entry(self.root, textvariable=self.pwd).grid(row=2, column=1)
+        self.button = tk.Button(self.root,text = "login",command = self.login, bg="#742831", fg="white").grid(row = 4, column = 1, pady=5, padx=5)
+        self.root.mainloop()
+
+    def get_user(self):
+        return self.__Username
+
+    def get_password(self):
+        return  self.__Password
+
+    def login(self):
+        self.user1 = self.get_user()
+        self.pwd1 = self.get_password()
+        self.users = self.user.get()
+        self.pwds = self.pwd.get()
+        if self.user1 == self.users and self.pwd1 == self.pwds:
+            self.root.destroy()
+            self.update()
+        else:
+            messagebox.showwarning("your user or password is invalid")
+
+    def update(self):
+        self.root1 = tk.Tk()
+        self.id = tk.StringVar()
+        self.title = tk.StringVar()
+        self.edition = tk.StringVar()
+        self.author = tk.StringVar()
+        self.book = tk.StringVar()
+        self.root1.title("Book Updation")
+        self.root1.geometry("500x500")
+        self.root1.iconbitmap('C:/image/book_icon.ico')
+        bg = tk.PhotoImage(file="C:/image/background2.png")
+        self.label1 = tk.Label(self.root1, image=bg)
+        self.label1.place(x=0, y=0)
+
+        # Book ID
+        self.bklab1 = ttk.Label(self.root1, text="Book I'd : ", background="#A5A39E", font=("Times",12,"bold italic")).grid(row=1, column=0, padx=5, pady=5)
+        self.bkentry1 = ttk.Entry(self.root1, textvariable=self.id).grid(row=1, column=1)
+
+        # Title
+        self.bklab2 = ttk.Label(self.root1, text="Book Title : ", background="#A5A39E", font=("Times",12,"bold italic")).grid(row=2, column=0, padx=5, pady=5)
+        self.bkentry2 = ttk.Entry(self.root1, textvariable=self.title).grid(row=2, column=1)
+
+        #Book Edition
+        self.bklab = ttk.Label(self.root1, text = "Book version :", background="#A5A39E", font=("Times",12,"bold italic")).grid(row=3, column = 0, padx=5, pady=5)
+        self.bkentry = ttk.Entry(self.root1,textvariable=self.edition).grid(row=3, column=1)
+
+        # Book Author
+        self.bklab3 = ttk.Label(self.root1, text="Author name : ", background="#A5A39E", font=("Times",12,"bold italic")).grid(row=4, column=0, padx=5, pady=5)
+        self.bkentry3 = ttk.Entry(self.root1, textvariable=self.author).grid(row=4, column=1)
+
+        # Book Browse
+        self.bklab4 = ttk.Label(self.root1, text="Book : ", background="#A5A39E", font=("Times",12,"bold italic")).grid(row=5, column=0, padx=5, pady=5)
+
+        # Browse button
+        self.browsebutton = tk.Button(self.root1, text="Browse", command=self.browsefunc, bg="#7E7A8B").grid(row=5, column=1, padx=5, pady=5)
+
+        # Submit Button
+        self.SubmitBtn = tk.Button(self.root1, text="Submit", command=self.bookRegister, bg="#7E7A8B").grid(row=6, column=0, pady=5)
+        self.deletebtn = tk.Button(self.root1, text="Delete", command=self.Delete, bg="#7E7A8B").grid(row=6, column=1, pady=5)
+        self.quitBtn = tk.Button(self.root1, text="Quit", command=self.root1.destroy, bg="#7E7A8B").grid(row=7, column=0, pady=5)
+        self.viewbtn = tk.Button(self.root1,text="View", command=self.view, bg="#7E7A8B").grid(row=6, column=2,pady=5)
+        self.root1.mainloop()
+
+    #browse function
+    def browsefunc(self):
+        self.filename = filedialog.askopenfilename(initialdir = "/", title = "select a file", filetype = (("pdf","*.pdf"), ("all Files","*.*")))
+        self.label = ttk.Label(self.root1,text = self.filename).grid(row = 5, column = 2)
+        with open(self.filename, 'rb') as file:
+            self.data = file.read()
+
+
+    def bookRegister(self):
+
+        # self.con = mysql.connector.connect(host="localhost", port = 3306, user="root", password="Roselie@2000", database="Ebooks")
+        # self.cur = con.cursor()
+        bid = self.id.get()
+        title = self.title.get()
+        ed = self.edition.get()
+        author = self.author.get()
+        book_file = str(self.filename)
+        insertBooks = "INSERT INTO books (book_id,book_title,book_edition,Author,path,book) VALUES (%s, %s, %s, %s, %s, %s)"
+        values = (bid,title,ed,author,self.filename,book_file)
+        try:
+            self.cur.execute(insertBooks,values)
+            self.con.commit()
+            messagebox.showinfo('Success', "Book added successfully")
+        except:
+            messagebox.showinfo("Error", "Can't add data into Database")
+
+    def view(self):
+
+        #create tkinter window
+        self.bkwin = tk.Tk()
+        self.bkwin.geometry("800x500")
+        self.bkwin.title("Books")
+        self.bkwin.iconbitmap('C:/image/book_icon.ico')
+        self.bkwin.config(bg="#D8BB71")
+
+        #create database connectivity
+        self.cur.execute("SELECT book_id,book_title,book_edition,Author FROM books")
+        self.fetch_row = self.cur.fetchall()
+
+        #table Heading
+        self.lab1 = ttk.Label(self.bkwin,text="Book id",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=0)
+        self.lab2 = ttk.Label(self.bkwin, text="Book title",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=1)
+        self.lab3 = ttk.Label(self.bkwin, text="Edition",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=2)
+        self.lab4 = ttk.Label(self.bkwin, text="Author",foreground="red", background="#D8BB71", font=("Times",12,"bold italic")).grid(row=0, column=3)
+
+        #display
+        i = 1
+        for bks in self.fetch_row:
+            for j in range(len(bks)):
+                self.bken = ttk.Label(self.bkwin, text=bks[j], borderwidth=10, relief='flat', anchor='w',
+                                      padding=5, background="#D8BB71", font=("Times",12)).grid(row=i, column=j)
+            i = i + 1
+
+    def Delete(self):
+
+        bid = self.id.get()
+        try:
+            self.cur.execute("delete from books where book_id ='" + bid + "'")
+            self.con.commit()
+            messagebox.showinfo('Success', "Book deleted successfully")
+        except:
+            messagebox.showinfo("Error", "Can't delete data into Database")
+
+app = Administrator()
+app.tkin1()
+
+
+
+
+
+
---
+++ User.py
@@ -0,0 +1,137 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+import mysql.connector
+import webbrowser as wb
+
+class User:
+    def __init__(self):
+
+        self.con = mysql.connector.connect(host="localhost", user="root", password="Roselie@2000", database="ebooks")
+        self.cur = self.con.cursor()
+
+    def tkin2(self):
+
+        self.root2 = tk.Tk()
+        self.mail = tk.StringVar()
+        self.pwd = tk.StringVar()
+        self.root2.title("User")
+        self.root2.geometry('500x400')
+        self.root2.iconbitmap('C:/image/book_icon.ico')
+        bg = tk.PhotoImage(file="C:/image/background1.png")
+        self.label1 = tk.Label(self.root2, image=bg)
+        self.label1.place(x=0, y=0)
+        self.userlab5 = ttk.Label(self.root2,text = "Email :", background="#61626B", font=("Times",14,"bold italic"), foreground="white").grid(row=3, column=0,pady=5)
+        self.userentry5 = ttk.Entry(self.root2,textvariable = self.mail).grid(row=3,column=1,pady=5)
+        self.userlab6 = ttk.Label(self.root2, text="Password :", background="#61626B", font=("Times",14,"bold italic"), foreground="white").grid(row=4, column=0,pady=5)
+        self.userentry6 = ttk.Entry(self.root2, textvariable=self.pwd).grid(row=4, column=1,pady=5)
+        self.button1 = tk.Button(self.root2,text = "login",command =self.login, bg="#742831").grid(row = 5, column = 1,pady=5)
+        self.info = ttk.Label(self.root2, text="Are you a new user?",background="#61626B", font=("Times",12,"bold italic"), foreground="white").grid(row=7, column=0, pady=5, padx=2)
+        self.button2 = tk.Button(self.root2,text="Sign in",command = self.UserRegister, bg="#742831").grid(row = 7,column = 1,pady=5)
+        self.root2.mainloop()
+
+    def UserRegister(self):
+
+        mail1 = self.mail.get()
+        pwds = self.pwd.get()
+        insertUsers = "INSERT INTO user (email,password) VALUES (%s, %s)"
+        values = (mail1,pwds)
+        try:
+            self.cur.execute(insertUsers,values)
+            self.con.commit()
+            messagebox.showinfo('Success', "You are Signup successfully")
+            self.view()
+        except:
+            self.con.rollback()
+            messagebox.showinfo("Error", "Can't add data into Database")
+            self.con.close
+
+    def login(self):
+        mail1 = self.mail.get()
+        pwds = self.pwd.get()
+        if mail1=="" or pwds=="":
+            messagebox.showinfo("please enter your mail id and password")
+        else:
+            self.cur.execute("select * from user where email = %s and password = %s",(mail1,pwds))
+            row = self.cur.fetchone()
+            if row == None:
+                messagebox.showinfo("Your email id or password is invalid")
+            else:
+                self.view()
+
+    def view(self):
+        self.root2.destroy()
+        # self.root3.destroy()
+
+        #create tkinter window
+        self.bkwin = tk.Tk()
+        self.search = tk.StringVar()
+        self.bkwin.geometry("800x500")
+        self.bkwin.title("Books")
+        self.bkwin.iconbitmap('C:/image/book_icon.ico')
+        self.bkwin.config(bg="#D8BB71")
+
+        #create database connectivity
+        self.cur.execute("SELECT book_id,book_title,book_edition,Author FROM books")
+        self.fetch_row = self.cur.fetchall()
+
+        #table Heading
+        self.lab1 = ttk.Label(self.bkwin,text="Book id",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=0)
+        self.lab2 = ttk.Label(self.bkwin, text="Book title",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=1)
+        self.lab3 = ttk.Label(self.bkwin, text="Edition",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=2)
+        self.lab4 = ttk.Label(self.bkwin, text="Author",foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=3)
+        self.ent1 = ttk.Entry(self.bkwin, textvariable=self.search).grid(row=0, column=7)
+        self.bt2 = tk.Button(self.bkwin, text="Search",fg="white", bg="maroon", relief="groove",
+                             command=self.Search).grid(row=0, column=8)
+        #display
+        i = 1
+        for bks in self.fetch_row:
+            for j in range(len(bks)):
+                self.bken = ttk.Label(self.bkwin, text=bks[j], borderwidth=10, relief='flat', anchor='w',
+                                      padding=5, background="#D8BB71", font=("Times",14,"bold italic")).grid(row=i, column=j)
+            self.bt = tk.Button(self.bkwin, text="View", fg="white", bg="green", relief="ridge",
+                                command=lambda row=i, column=j: self.read(row, column)).grid(row=i,column=j + 1)
+            i = i + 1
+
+    def Search(self):
+        self.bkwin1 = tk.Tk()
+        self.bkwin1.geometry("800x500")
+        self.bkwin1.title("Books")
+        self.bkwin1.iconbitmap('C:/image/book_icon.ico')
+        self.bkwin1.config(bg="#D8BB71")
+        data = self.search.get()
+        data = self.search.get()
+        self.cur.execute("select book_id,book_title,book_edition,Author from books where book_title ='" + data + "'")
+        self.fetch_row = self.cur.fetchall()
+        if self.fetch_row == None:
+            messagebox.showinfo("The book is currently not present in the database")
+        else:
+            self.lab1 = ttk.Label(self.bkwin1, text="Book id", foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=0)
+            self.lab2 = ttk.Label(self.bkwin1, text="Book title", foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=1)
+            self.lab3 = ttk.Label(self.bkwin1, text="Edition", foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=2)
+            self.lab4 = ttk.Label(self.bkwin1, text="Author", foreground="red", background="#D8BB71", font=("Times",14,"bold italic")).grid(row=0, column=3)
+        i = 1
+        for bks in self.fetch_row:
+            for j in range(len(bks)):
+                self.bken = ttk.Label(self.bkwin1, text=bks[j], borderwidth=10, relief='flat', anchor='w',padding=5, background="#D8BB71", font=("Times",14,"bold italic")).grid(row=i, column=j)
+            self.bt = tk.Button(self.bkwin1, text="View", fg="white", bg="green", relief="ridge", command=lambda row=i, column=j: self.read(row, column)).grid(row = i,column=j+1)
+            i = i + 1
+
+
+    def read(self,row,column):
+        bk_info = self.fetch_row[row-1]
+        id = bk_info[0]
+        title = bk_info[1]
+        ed = bk_info[2]
+        blob_query = "select path from books where book_id = %s or book_title = %s and book_edition= %s"
+        values = (id,title,ed)
+        self.cur.execute(blob_query,values)
+        fetched_row = self.cur.fetchall()
+        data = str(fetched_row[0][0])
+        filename = data
+        wb.open_new(filename)
+
+app1 = User()
+app1.tkin2()
+
